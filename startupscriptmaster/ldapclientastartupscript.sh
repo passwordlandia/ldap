d6c68bb932058fd209310384afda477f454e7bd8
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-05-31 12:59:50 -0700

    Update ldapclientastartupscript.sh

--- startupscriptmaster/ldapclientastartupscript.sh
+++ startupscriptmaster/ldapclientastartupscript.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+apt-get --yes install libpam-ldap nscd
+
+sed -i -r 's " 
+/etc/init.d/nscd restart
+
+sed -i -r 's /etc/ssh/sshd_config "PasswordAuthentication/no PasswordAuthentication/yes" g'
+
+/etc/init.d/ssh restart
+
+#sed -i 's,uri ldapi:///,uri ldap://youriphere,g' /etc/ldap.conf
+
+#sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
