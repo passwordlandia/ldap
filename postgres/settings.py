4dca2eb42cde3a8716b3d3de47cd11b2c4e93c94
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-10 04:48:29 -0700

    Update settings.py

--- postgres/settings.py
+++ postgres/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'myproject',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
-        'HOST': '10.142.0.22',
+        'HOST': '10.142.0.12',
         'PORT': '5432',
     }
 }
