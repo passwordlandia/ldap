0a79cbc9421d668970ec4a3aa6b503d5a65240be
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-12 19:36:41 -0700

    Update ldapclientastartupscript.sh

--- startupscriptmaster/ldapclientastartupscript.sh
+++ startupscriptmaster/ldapclientastartupscript.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+
 apt-install -y debconf-utils
 wget https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/ldap_debconf
+
+ldapip=$(gcloud compute instances list | grep ldap-server-a | awk '{ print $4 }' | tail -1) 
+
+sed -i 's,ldap://(youriphere),ldap://"$ldapip",g' /ldap_debconf
 
 while read line; do echo "$line" | debconf-set-selection; < ldap_debconf
 
@@ -13,8 +18,3 @@ sed -i 's/compat/compat ldap/g' /etc/nsswitch.conf
 sed -i 's/PasswordAuthentication no/PasswordAuthentication Yes/g' /etc/ssh/sshd_config
 
 /etc/init.d/nscd restart
-
-
-#sed -i 's,uri ldapi:///,uri ldap://youriphere,g' /etc/ldap.conf
-
-#sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
