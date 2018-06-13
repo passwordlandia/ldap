8a7cf3b80a682901235826e593cfb3561b49040d
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-12 18:47:46 -0700

    Update ldapclientastartupscript.sh
    added some fully automated client code to nonworking automated code. This is ready to add in nfs into it

--- startupscriptmaster/ldapclientastartupscript.sh
+++ startupscriptmaster/ldapclientastartupscript.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-apt-get --yes install libpam-ldap nscd
+export DEBIAN_FRONTEND=noninteractive
+apt-install -y debconf-utils
+wget https://raw.githubusercontent.com/passwordlandia/ldap/master/ldap_debconf
 
-sed -i -r 's " 
+while read line; do echo "$line" | debconf-set-selection; < ldap_debconf
+
+apt-get -y install libpam-ldap nscd
+
+sed -i 's/compat/compat ldap/g' /etc/nsswitch.conf
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication Yes/g' /etc/ssh/sshd_config
+
 /etc/init.d/nscd restart
 
-sed -i -r 's /etc/ssh/sshd_config "PasswordAuthentication/no PasswordAuthentication/yes" g'
-
-/etc/init.d/ssh restart
 
 #sed -i 's,uri ldapi:///,uri ldap://youriphere,g' /etc/ldap.conf
 
