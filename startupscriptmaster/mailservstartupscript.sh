3ea69ed4d973ad71bea5dcc5fc10287e44e9d620
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-13 18:40:53 -0700

    Update mailservstartupscript.sh

--- startupscriptmaster/mailservstartupscript.sh
+++ startupscriptmaster/mailservstartupscript.sh
@@ -19,6 +19,7 @@ $LOCALITY
 $ORGNAME
 $ORGUNIT
 $HOSTNAME
+
 $EMAIL
 __EOF__
 
@@ -29,45 +30,20 @@ yum -y install wget
 
 ####Then wget the exim config file: you can check the contents here:/etc/exim/exim.conf####
 
-wget https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/exim.conf
-mv -f /root/SSL/mail.mydomain.com/exim.conf /etc/exim/exim.conf
-
-cd /root/SSL/mail.mydomain.com
+curl https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/exim.conf > /etc/exim/exim.conf
 
 systemctl start exim
 systemctl status exim
 systemctl enable exim
 
-yum -y install dovecot
-
-sed -i 's"ssl = required/ssl = yes"/g' /etc/dovecot/conf.d/10-ssl.conf
-sed -i "ssl_cert = </etc/pki/dovecot/certs/dovecot.pem;ssl_cert = </etc/ssl/mail.mydomain.com.crt";/g' /etc/dovecot/conf.d/10-ssl.conf
-sed -i "ssl_key = </etc/pki/dovecot/private/dovecot.pem;ssl_key = </etc/ssl/mail.mydomain.com.key";g' /etc/dovecot/conf.d/10-ssl.conf
-
-sed -i 's/"#disable_plaintext_auth = yes/disable_plaintext_auth = no"/g' /etc/dovecot/conf.d/10-auth.conf
-sed -i 's"auth_mechanisms = plain/auth_mechanisms = plain login"/g' /etc/dovecot/conf.d/10-auth.conf
-
-sed -i "s;#mail_location = ;mail_location = maildir:~/Maildir;g" /etc/dovecot/conf.d/10-mail.conf
-
-echo "service auth {
-...
-    unix_listener auth-client {
-        mode = 0660
-        user = exim
-    }
-}" >> /etc/dovecot/conf.d/10-master.conf
+curl https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/10-ssl.conf
+curl https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/10-auth.conf
+curl https://raw.githubusercontent.com/passwordlandia/ldap/master/startupscriptmaster/10-mail.conf
+curl https://github.com/passwordlandia/ldap/blob/master/startupscriptmaster/10-master.conf
 
 systemctl start dovecot
 systemctl status dovecot
 systemctl enable dovecot
 
-echo "test" | /usr/sbin/exim -v paulierev1775@mailserv-test
+echo "test" | /usr/sbin/exim -v paulierev1775@mydomain.com
 
-###To test: echo "test" | /usr/sbin/exim -v nicolebade@mydomain.com where nicolebade@mydomain.com is your gcloud user,### 
-###you should see exam working to deliver the message. It won't go through because of port blockage,### 
-###but you'll be able to see it in the output of your command.###
-
-###if you tail /var/log/exim/main.log you should see the mail attempting to be sent and timing out:###
-
-###Complete this assignment by automating your build of the mail server.###
-If you need help, don't hesitate to ask. Note that "wgeting" exim and dovecot config files is fine to complete the configuration.###
