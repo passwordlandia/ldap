26bfbe496068f1a583d331234b720d10047f292e
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-14 17:51:50 -0700

    added echo line for repo

--- startupscriptmaster/ubnfsclientstartupscript.sh
+++ startupscriptmaster/ubnfsclientstartupscript.sh
@@ -29,3 +29,12 @@ mkdir /mnt/test
 echo "$nfsip:/var/nfsshare/testing /mnt/test nfs defaults 0 0" >> /etc/fstab 
 mount -a
 *profit*
+
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.142.0.19/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo 
