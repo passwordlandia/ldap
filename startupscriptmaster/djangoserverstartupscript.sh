171c0947dd3113dbd36edc9eb0df2154ba0b114b
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-14 17:51:22 -0700

    added echo line for repo
    and added setenforce 0 not sure why yet :)

--- startupscriptmaster/djangoserverstartupscript.sh
+++ startupscriptmaster/djangoserverstartupscript.sh
@@ -19,3 +19,14 @@ source myprojectenv/bin/activate && pip install django psycopg2 && django-admin.
 mv -f /opt/myproject/settings.py /opt/myproject/myproject/settings.py
 
 source myprojectenv/bin/activate && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+
+setenforce 0
+
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.142.0.19/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" >> /etc/yum.repos.d/NTI-320.repo 
